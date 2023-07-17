3ce1ebc9f63ef36bae084809bf98a4a50fd0cd11
Daniel Santana <danielsantanachastel@gmail.com>
2023-07-17 15:34:25 -0300

    adicionando funcionalidades

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -20,19 +20,21 @@ export const Content = ()=> {
             <p className="text-sm text-zinc-400 mb-6 sm:mb-16 sm:text-lg">IMC é a sigla para índice da massa corpórea, parâmetro adotado pela Organização Mundial da Saúde para calcular o peso ideal de cada pessoa</p>
             <input
             type="number"
-            className="outline-none text-sm sm:text-lg bg-none border-b p-3 border-zinc-300 w-full mb-4 sm:mb-8 text-zinc-500 hover:border-blue-400 hover:placeholder:text-blue-400 hover:text-blue-400 focus:border-blue-400 transition-all"
+            className="outline-none text-sm sm:text-lg bg-none border-b p-3 border-zinc-300 w-full mb-4 sm:mb-8 text-zinc-500 hover:border-blue-400 hover:placeholder:text-blue-400 hover:text-blue-400 focus:border-blue-400 disabled:bg-white disabled:opacity-50 disabled:border-zinc-300 disabled:text-zinc-500 transition-all"
             placeholder="Digite sua altura. Ex: 1.5 (em metros)"
             onChange={e=>setHeightInput(e.target.value)}
             value={heightInput}
+            disabled={imcCtx?.imc !== 0 && imcCtx?.imc !== null? true : false}
             />
             <input 
             type="number"
-            className="spin outline-none text-sm sm:text-lg bg-none border-b p-3 border-zinc-300 w-full mb-4 sm:mb-8 text-zinc-500 hover:border-blue-400 hover:text-blue-400 hover:placeholder:text-blue-400 focus:border-blue-400 transition-all" 
+            className="spin outline-none text-sm sm:text-lg bg-none border-b p-3 border-zinc-300 w-full mb-4 sm:mb-8 text-zinc-500 hover:border-blue-400 hover:text-blue-400 hover:placeholder:text-blue-400 focus:border-blue-400 disabled:bg-white disabled:opacity-50 disabled:border-zinc-300 disabled:text-zinc-500 transition-all" 
             placeholder="Digite o seu peso. Ex: 47.7 (em kg)"
             onChange={e=>setWeightInput(e.target.value)}
             value={weightInput}
+            disabled={imcCtx?.imc !== 0 && imcCtx?.imc !== null? true : false}
             />
-            <button onClick={   handleCalcButton} className="w-full py-3 text-white bg-blue-400 rounded-lg mt-4 sm:mt-10 hover:bg-blue-500 transition-all">Calcular</button>
+            <button onClick={handleCalcButton} disabled={imcCtx?.imc !== 0 && imcCtx?.imc !== null? true : false} className="w-full py-3 text-white bg-blue-400 rounded-lg mt-4 sm:mt-10 hover:bg-blue-500 disabled:opacity-50 disabled:hover:bg-blue-400 transition-all">Calcular</button>
         </div>
     )
 }
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -16,16 +16,17 @@ export const Box = ({url, color, mainLabel, label}: Props)=> {
         <>
         {imcCtx?.imc === 0 &&
         <div className={` flex flex-col items-center justify-center rounded-xl px-2 md:px-0 ${color}`}>
-        <div className={`w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full bg-black/20 my-4`}><img src={url} className="w-7 md:w-9"/></div>
-        <h2 className="text-white font-bold text-xl md:text-2xl mb-4">{mainLabel}</h2>
-        <p className="text-white text-xs text-center md:text-sm mb-4">IMC está entre {label}</p>
+            <div className={`w-16 h-16 md:w-20 md:h-20 flex items-center justify-center rounded-full bg-black/20 my-4`}><img src={url} className="w-7 md:w-9"/></div>
+            <h2 className="text-white font-bold text-xl md:text-2xl mb-4">{mainLabel}</h2>
+            <p className="text-white text-xs text-center md:text-sm mb-4">IMC está entre {label}</p>
         </div>
         }
         {imcCtx?.imc !== 0 && imcCtx?.imc !== null &&
-            <div className={`w-full h-full flex relative flex-col items-center justify-center rounded-xl mt-3 py-10 lg:py-0 ${color}`}>
+            <div className={`flex-1 flex relative flex-col items-center justify-center rounded-xl mt-3 py-10 lg:py-0 ${color}`}>
                 <div className={`w-24 h-24 flex items-center justify-center rounded-full bg-black/20 my-4`}><img src={url} className="w-14"/></div>
-                <h2 className="text-white font-bold text-2xl mb-4">{mainLabel}</h2>
-                <p className="text-white text-sm">IMC está entre {label}</p>
+                <h2 className="text-white font-bold text-2xl lg:text-4xl mb-4">{mainLabel}</h2>
+                <p className="text-white mb-8 text-sm lg:text-xl">Seu IMC é de <strong>{imcCtx?.imc.toFixed(2)} kg/m²</strong></p>
+                <p className="text-white text-xs lg:text-lg">IMC está entre {label}</p>
                 <div onClick={()=>imcCtx?.setImc(0)} className="lg:h-16 lg:w-16 h-12 w-12 rounded-full bg-blue-400 absolute top-0 -mt-6 lg:left-0 lg:top-1/2 lg:-ml-8 flex items-center justify-center hover:bg-blue-500 transition-all cursor-pointer"><img  src="assets/leftarrow.png" className="lg:w-7 w-4" alt="" /></div>
             </div>
         }
